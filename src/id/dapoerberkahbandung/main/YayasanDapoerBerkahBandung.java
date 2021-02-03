17bd442e1c1ac9327c3f6dc1036702a15b3de7ed
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2021-02-03 15:36:16 +0700

    membuat main class

---
+++ src/id/dapoerberkahbandung/main/YayasanDapoerBerkahBandung.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.dapoerberkahbandung.main;
+
+import id.dapoerberkahbandung.error.AnggotaException;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author Dandi Ahmadin
+ */
+public class YayasanDapoerBerkahBandung {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException, AnggotaException {
+        // TODO code application logic here
+    }
+    
+}
