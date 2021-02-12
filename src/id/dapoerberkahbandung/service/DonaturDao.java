f8f4b4b2c442a263a656f0be9204567bc5827563
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2021-02-12 14:27:25 +0700

    menambahkan interface donatur

---
+++ src/id/dapoerberkahbandung/service/DonaturDao.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.dapoerberkahbandung.service;
+
+import id.dapoerberkahbandung.entity.Donatur;
+import id.dapoerberkahbandung.error.DonaturException;
+import java.util.List;
+
+/**
+ *
+ * @author Dandi Ahmadin
+ */
+public interface DonaturDao {
+    public void autoGenerateId();
+    
+    public void insertDonatur(Donatur donatur) throws DonaturException;
+    public void updateDonatur(Donatur donatur) throws DonaturException;
+    public void deleteDonatur() throws DonaturException;
+    public void getDonatur(String id_anggota) throws DonaturException;
+    public List<Donatur> selectAllDonatur() throws DonaturException;
+    
+}
