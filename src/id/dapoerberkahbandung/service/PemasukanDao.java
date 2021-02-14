a2adfd5a5c4f447e845e121a3bc81687b7763f02
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2021-02-14 14:56:44 +0700

    membuat interface pemasukan

---
+++ src/id/dapoerberkahbandung/service/PemasukanDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.dapoerberkahbandung.service;
+
+import id.dapoerberkahbandung.entity.Pemasukan;
+import id.dapoerberkahbandung.error.PemasukanException;
+import java.util.List;
+
+/**
+ *
+ * @author Dandi Ahmadin
+ */
+public interface PemasukanDao {
+    public void insertPemasukan(Pemasukan pemasukan) throws PemasukanException;
+    public void updatePemasukan(Pemasukan pemasukan) throws PemasukanException;
+    public void deletePemasukan(String no_pemasukan) throws PemasukanException;
+    public Pemasukan getPemasukan(String no_pemasukan) throws PemasukanException;
+    public List<Pemasukan> selectAllPemasukan() throws PemasukanException;
+}
