14b555822e20541b5f2e5f6e0d01d2edf349e479
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2021-02-15 22:17:27 +0700

    menambahkan fungsi interface onChange, onInsert, onUpdate, onDelete

--- src/id/dapoerberkahbandung/event/PengeluaranListener.java
+++ src/id/dapoerberkahbandung/event/PengeluaranListener.java
@@ -5,10 +5,16 @@
  */
 package id.dapoerberkahbandung.event;
 
+import id.dapoerberkahbandung.entity.Pengeluaran;
+import id.dapoerberkahbandung.model.PengeluaranModel;
+
 /**
  *
  * @author Alfi Nurizkya
  */
 public interface PengeluaranListener {
-    
+    public void onChange(PengeluaranModel model);
+    public void onInsert(Pengeluaran pengeluaran);
+    public void onUpdate(Pengeluaran pengeluaran);
+    public void onDelete();
 }
