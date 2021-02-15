7388bb79302ce887a1554e559e3b01c046c208a7
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2021-02-15 21:26:30 +0700

    menambahkan interface untuk pengeluaran insert, delete, update, getPengeluaran, selectAll

--- src/id/dapoerberkahbandung/service/PengeluaranDao.java
+++ src/id/dapoerberkahbandung/service/PengeluaranDao.java
@@ -5,10 +5,18 @@
  */
 package id.dapoerberkahbandung.service;
 
+import id.dapoerberkahbandung.entity.Pengeluaran;
+import id.dapoerberkahbandung.error.PengeluaranException;
+import java.util.List;
+
 /**
  *
  * @author Alfi Nurizkya
  */
 public interface PengeluaranDao {
-    
+    public void insertPengeluaran(Pengeluaran pengeluaran) throws PengeluaranException;
+    public void updatePengeluaran(Pengeluaran pengeluaran) throws PengeluaranException;
+    public void deletePengeluaran(int no_pengeluaran) throws PengeluaranException;
+    public Pengeluaran getPengeluaran(int no_pengeluaran) throws PengeluaranException;
+    public List<Pengeluaran> selectAllPengeluaran() throws PengeluaranException;
 }
